8a7c68b635d04152c3e0c7293ebf4f3e65d323b9
Claudiu <claudiu.ciuca95@gmail.com>
2026-05-10 17:23:58 +0300

    Fetch and Display dogs data

--- src/pages/Dogs.tsx
+++ src/pages/Dogs.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+
+
 function Dogs() {
 
-    return <h1>Dogs</h1>;
-    
+ const [dogs, setDogs] = useState([]);
+ useEffect(() => {
+    const fetchDogs = async () => {
+        try {
+            const response = await fetch("http://localhost:3001/dogs");
+            if (!response.ok) {
+                throw new Error("Failed to fetch dogs");
+            }
+
+            const data = await response.json();
+
+            setDogs(data);
+        }
+        catch (error) {
+            console.log(error);
+            
+        }
+        
+    };
+
+    fetchDogs();
+}, []);
+
+return (
+    <div> 
+        <h1> Dogs looking for a home</h1>
+
+        {dogs.map((dog: any) =>
+         <div key={dog.id}>
+            <h2>{dog.name}</h2>
+            <p>{dog.breed}</p>
+            <img src={dog.image} alt={dog.name} width="250px"/>
+        </div>
+        
+        )}
+    </div>
+)
+
+
 }
 
 export default Dogs;
